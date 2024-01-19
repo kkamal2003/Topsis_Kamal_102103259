38389ae5bdbf18f41e4c9065fda7f9489fa6e5e8
Kamalpreet kaur <126082752+kkamal2003@users.noreply.github.com>
2024-01-19 20:46:20 +0530

    commit

--- Topsis_Kamal_102103259/__main__.py
+++ Topsis_Kamal_102103259/__main__.py
@@ -38,14 +38,14 @@ def TOPSIS(input_file,weights,impacts,output_file):
         
 
     
-# if __name__ == "__main__":
-#     if len(sys.argv) != 5:
-#         print("incorrect number of parameters")
-#         sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("incorrect number of parameters")
+        sys.exit(1)
     
-#     input_file = sys.argv[1]
-#     weights = sys.argv[2]
-#     impacts = sys.argv[3]
-#     output_file = sys.argv[4]
+    input_file = sys.argv[1]
+    weights = sys.argv[2]
+    impacts = sys.argv[3]
+    output_file = sys.argv[4]
     
-#     TOPSIS(input_file,weights,impacts,output_file)
+    TOPSIS(input_file,weights,impacts,output_file)
